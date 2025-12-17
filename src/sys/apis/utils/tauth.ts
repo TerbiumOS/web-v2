09c33dbf8d4b90d16042f654272e6cc412ee7468
John <72810050+Notplayingallday383@users.noreply.github.com>
2025-12-17 14:37:57 +0000

    improvements to auth & cleanup

--- src/sys/apis/utils/tauth.ts
+++ src/sys/apis/utils/tauth.ts
@@ -4,58 +4,64 @@ export const auth = createAuthClient({
 	baseURL: "https://auth.terbiumon.top",
 });
 
-export async function getinfo(user: string, pass: string, setting?: string) {
-	await auth.signIn.email({
-		email: user,
-		password: pass,
+export async function getinfo(user?: string, pass?: string, setting?: string) {
+	if (user && pass) {
+		console.log("[TAUTH] Signing in with provided credentials...");
+		await auth.signIn.email({
+			email: user,
+			password: pass,
+		});
+	}
+
+	const response = await fetch("https://auth.terbiumon.top/user/info", {
+		credentials: "include",
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
 	});
+	const uinf = response.ok ? await response.json() : { error: "Failed to fetch user info" };
 
-	const uinf = (
-		await fetch("https://auth.terbiumon.top/user/info", {
-			credentials: "include",
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-	).json();
-
-	const settings = setting
-		? await (
-				await fetch(`https://auth.terbiumon.top/kv/retrieve/${setting}`, {
-					credentials: "include",
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				})
-			).json()
+	const sr = setting
+		? await fetch(`https://auth.terbiumon.top/kv/retrieve/${setting}`, {
+				credentials: "include",
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
 		: null;
+	const settings = sr === null ? null : sr.ok ? await sr.json() : { error: "Failed to fetch settings" };
 
+	console.log(uinf, settings);
 	return {
 		user: uinf,
 		settings: settings,
 	};
 }
 
-export async function setinfo(user: string, pass: string, setting: string, toset: any) {
-	await auth.signIn.email({
-		email: user,
-		password: pass,
+export async function setinfo(user?: string, pass?: string, setting?: string, toset?: any) {
+	if (user && pass) {
+		console.log("[TAUTH] Signing in with provided credentials...");
+		await auth.signIn.email({
+			email: user,
+			password: pass,
+		});
+	}
+	if (!setting || !toset) {
+		return { error: "No setting or value to set provided" };
+	}
+
+	const response = await fetch(`https://auth.terbiumon.top/kv/set/${setting}`, {
+		credentials: "include",
+		method: "POST",
+		body: JSON.stringify({
+			value: JSON.stringify({ toset }),
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
 	});
-
-	const changer = (
-		await fetch(`https://auth.terbiumon.top/kv/set/${setting}`, {
-			credentials: "include",
-			method: "POST",
-			body: JSON.stringify({
-				value: JSON.stringify({ toset }),
-			}),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-	).json();
-
+	const changer = response.ok ? await response.json() : { error: "Failed to set info" };
 	return changer;
 }
